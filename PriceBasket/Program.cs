8b475a6ae3d0708dcfe8917911bacddfd284943d
unknown-user <unknown-user@192.168.0.6>
2017-06-11 00:01:35 +0100

    Initial check-in of module PriceBasket

---
+++ PriceBasket/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceBasket
+{
+	public interface IGoods
+	{
+		string GetName();
+		double GetPrice();
+		double GetDiscount();
+	}
+	// Define my goods - which all implement the IGoods interface
+	public class Soup : IGoods
+	{
+		#region IGoods members
+		public string GetName()
+		{
+			return "Soup";
+		}
+		public double GetPrice()
+		{
+			return 0.65;
+		}
+		public double GetDiscount()
+		{
+			return 0;
+		}
+		#endregion
+	}
+	public class Bread : IGoods
+	{
+		#region IGoods members
+		public string GetName()
+		{
+			return "Bread";
+		}
+		public double GetPrice()
+		{
+			return 0.80;
+		}
+		public double GetDiscount()
+		{
+			return 0;
+		}
+		#endregion
+	}
+	public class Milk : IGoods
+	{
+		#region IGoods members
+		public string GetName()
+		{
+			return "Milk";
+		}
+		public double GetPrice()
+		{
+			return 1.30;
+		}
+		public double GetDiscount()
+		{
+			return 0;
+		}
+		#endregion
+	}
+	public class Apples : IGoods
+	{
+		#region IGoods members
+		public string GetName()
+		{
+			return "Apples";
+		}
+		public double GetPrice()
+		{
+			return 1.00;
+		}
+		public double GetDiscount()
+		{
+			return 00.10;
+		}
+		#endregion
+	}
+
+
+	public class PriceBasket
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				//Create the correct goods object for each ingredient passed in the parameters
+				List<IGoods> goods = new List<IGoods>();
+				for (int i = 0; i < args.Length; i++)
+				{
+					//Don't forget to handle products that may be passed in any case. Handle this by converting to uppercase before checking.
+					switch (args[i].ToUpper())
+					{
+					case "SOUP" :
+						goods.Add(new Soup());
+						break;
+					case "BREAD" :
+						goods.Add(new Bread());
+						break;
+					case "MILK" :
+						goods.Add(new Milk());
+						break;
+					case "APPLES" :
+						goods.Add(new Apples());
+						break;
+					default :
+						throw new ArgumentException(args[i] + " is an invalid entry");
+					}
+				}
+				//Now that we have a list of goods objects generate the output from the products.
+				PriceOutput(goods);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+		}
+		//This function loops through the list of goods and works out the subtotal, any discounts and a discounted price.
+		private static void PriceOutput(List<IGoods> goods)
+		{
+			double subtotal = 0;
+			string discounttext = "";
+			double discount = 0;
+			int matchingitems = 0;
+
+			for (int i = 0; i < goods.Count(); i++)
+			{
+				subtotal += goods[i].GetPrice();
+				if(goods[i].GetDiscount() > 0)
+				{
+					//Update discounttext checking that we havenít already mentioned this item
+					if(!discounttext.Contains(goods[i].GetName()))
+					{
+						discounttext += goods[i].GetName() + " " + System.Convert.ToString(goods[i].GetDiscount() * 100) + "% off: - ";
+						//Add up the discount for any matching items
+						matchingitems = (from n in goods where n.GetName()==goods[i].GetName() select n).Count();
+						discount += goods[i].GetDiscount() * matchingitems;
+						discounttext += System.Convert.ToString(((goods[i].GetPrice() * 100) * goods[i].GetDiscount()) * matchingitems) + "p ";
+					}
+				}
+
+			}
+			discounttext += CombinationDiscount(goods, "Soup", 2, "Bread", 1, 00.50, ref discount);
+			//If we havenít found a discount then set discounttext to indicate this
+			if (discounttext.Length == 0)
+			{
+				discounttext = "(No offers available)";
+			}
+			//Output the subtotal
+			Console.WriteLine("Subtotal: " + string.Format("{0:C}",subtotal));
+
+			//Output the discount items
+			Console.WriteLine(discounttext);
+
+			//Output the total - Subtotal minus any discounts
+			Console.WriteLine("Total price: " + string.Format("{0:C}", subtotal - discount));
+			return;
+		}
+		//This function handles combination discounts. Combitem indicates the source combination items. Combamount indicates the
+		//number of the combination items needed to achieve a combination discount. Discitem indicates the item that recieves the
+		//combination discount. Discitemcount indicates how many items recieve a discount. DiscAmount indicates the amount of discount.
+		//discount (passed by reference) is the value of all discounts recieved and is updated by this method.
+		private static string CombinationDiscount(List<IGoods> goods, string combitem, int combamount, string discitem, int discitemcount, double discAmount, ref double discount)
+		{
+
+			int matchingitems = 0;
+			int matchingdiscounts = 0;
+			int applieddiscount = 0;
+			double combdisc = 0;
+			string discountresult = "";
+			if(discitemcount > 0)
+			{
+				matchingitems = (from n in goods where n.GetName() == combitem select n).Count();
+				matchingdiscounts = (from n in goods where n.GetName() == discitem select n).Count();
+
+				if (matchingitems >= combamount && matchingdiscounts > 0)
+				{
+					discountresult = discitem + " " + discitemcount.ToString() + " item " + System.Convert.ToString(discAmount * 100) + "% off: - ";
+					List<IGoods> discountItems = goods.Where(x => x.GetName() == discitem).ToList();
+					for (int i = 0; i < discountItems.Count(); i++)
+					{
+						combdisc += (discountItems[i].GetPrice() * discAmount);
+						applieddiscount = 1;
+						if (applieddiscount >= discitemcount)
+						{
+							break;
+						}
+					}
+					discountresult += System.Convert.ToString(combdisc * 100) + "p ";
+				}
+				discount += combdisc;
+			}
+			return discountresult;
+		}
+
+	}
+}
---
+++ Tests1/Test.cs
@@ -0,0 +1,124 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace Tests1
+{
+	/// <span class="code-SummaryComment"><summary></span>
+	/// Testing application BuildAdvantage.Console.exe
+	/// <span class="code-SummaryComment"></summary></span>
+	[TestFixture]
+	public class ProgramTests
+	{
+
+		TextWriter m_normalOutput;
+		StringWriter m_testingConsole;
+		StringBuilder m_testingSB;
+
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp()
+		{
+			try
+			{
+			// Set current folder to testing folder
+			string assemblyCodeBase = 
+				System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+
+			// Get directory name
+			string dirName = Path.GetDirectoryName(assemblyCodeBase);
+
+			// remove URL-prefix if it exists
+			if (dirName.StartsWith("file:\\"))
+				dirName = dirName.Substring(6);
+
+			// set current folder
+				dirName = "/Users/scothaynes/Projects/PriceBasket/PriceBasket/bin/debug";
+			Environment.CurrentDirectory = dirName;
+
+			// Initialize string builder to replace console
+			System.Console.Clear();
+			m_testingSB = new StringBuilder();
+			m_testingConsole = new StringWriter(m_testingSB);
+
+			// swap normal output console with testing console - to reuse 
+			// it later
+			m_normalOutput = System.Console.Out;
+			System.Console.SetOut(m_testingConsole);
+			}
+			catch (Exception ex)
+			{
+					string myerr = ex.ToString();
+			}
+		}
+		[TestFixtureTearDown]
+		public void TestFixtureTearDown()
+		{
+			// set normal output stream to the console
+			System.Console.SetOut(m_normalOutput);
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			// clear string builder
+			m_testingSB.Remove(0, m_testingSB.Length);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			// Verbose output in console
+			m_normalOutput.Write(m_testingSB.ToString());
+		}
+		private int StartConsoleApplication(string arguments)
+		{
+			Process proc = new Process();
+			try {
+			// Initialize process here
+			proc.StartInfo.FileName = "PriceBasket.exe";
+			// add arguments as whole string
+			proc.StartInfo.Arguments = arguments;
+
+			// use it to start from testing environment
+			proc.StartInfo.UseShellExecute = false;
+
+			// redirect outputs to have it in testing console
+			proc.StartInfo.RedirectStandardOutput = true;
+			proc.StartInfo.RedirectStandardError = true;
+
+			// set working directory
+			proc.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+
+			// start and wait for exit
+			proc.Start();
+			proc.WaitForExit();
+
+			// get output to testing console.
+			System.Console.WriteLine(proc.StandardOutput.ReadToEnd());
+			System.Console.Write(proc.StandardError.ReadToEnd());
+				// return exit code
+				return proc.ExitCode;
+
+		}
+			catch(Exception ex) 
+		{
+				string myerr = ex.ToString();
+				return 0;
+		}
+
+		}
+		[Test]
+		public void ShowCmdHelpIfNoArguments()
+		{
+			// Check exit is normal
+			Assert.AreEqual(0, StartConsoleApplication("Soup"));
+
+			// Check that help information shown correctly.
+			Assert.IsTrue(m_testingSB.ToString().Contains(
+				"Subtotal"));
+		}
+
+	}
+}
